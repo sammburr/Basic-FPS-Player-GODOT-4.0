8833e10b97674a0a1e069924f7ff69e08395b6d4
sammburr <53175463+sammburr@users.noreply.github.com>
2023-12-18 19:49:52 +0000

    Attempt #4 at fixing file structure

--- addons/Basic FPS Player/basic_fps_player.gd
+++ addons/Basic FPS Player/basic_fps_player.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("Basic FPS Player", "CharacterBody3D", preload("basic_player_startup.gd"), preload("Assets/Basic FPS Player.svg"))
+	add_custom_type("Basic FPS Player", "CharacterBody3D", preload("Src/basic_player_startup.gd"), preload("Assets/Basic FPS Player.svg"))
 
 func _exit_tree():
 	remove_custom_type("Basic FPS Player")
